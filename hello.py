66dc29fd3f70629ddd807c098193a380d78526b5
NeymarLiang <106260223+NeymarLiang@users.noreply.github.com>
2023-06-11 12:52:52 +0800

    Update hello.py
    add goodbye function

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 def hello(f):
     return f"Hello {f}"
 
+def goodbye(f):
+    return f"Goodbye {f}"
+
 def main():
     user = input("Name: ")
+
     print(hello(user))
+    print(goodbye(user))
 
 if __name__ == "__main__":
     main()
